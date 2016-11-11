a1148865a8e350da54957ce8f70250366bdb7776
Alvin1992 <fanrx_1992@163.com>
2016-11-11 10:23:59 -0800

    hello world

---
+++ nodejs-sample/hello.js
@@ -0,0 +1,16 @@
+/**
+ * Created by alvin on 11/11/16.
+ */
+
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type':'text/html; charset=utf-8'});
+    if (req.url !== "favicon.ico") {  // remove the second visit
+        console.log('visit');
+        res.write('Hello World');
+        res.end();  // end this visit otherwise the browser will stay loading
+    }
+}).listen(8080);
+
+console.log('Server running at http://127.0.0.1:8080');
