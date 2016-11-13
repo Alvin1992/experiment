5b1887953737b7f82f9e59313a7ae3b9e38f1e6f
Alvin1992 <fanrx_1992@163.com>
2016-11-13 06:54:57 -0800

    router

---
+++ nodejs-sample/router.js
@@ -0,0 +1,12 @@
+/**
+ * Created by alvin on 11/13/16.
+ */
+
+module.exports = {
+    login: function (res) {
+        res.write('This is the function of login');
+    },
+    register: function (res) {
+        res.write('Thiis is the function of register');
+    }
+};
---
+++ nodejs-sample/use_router.js
@@ -0,0 +1,23 @@
+/**
+ * Created by alvin on 11/13/16.
+ */
+
+var http = require('http');
+var router = require('./router');
+var url = require('url');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html; charset-utf-8'});
+    if (req.url !== 'favicon.ico') {
+        var pathname = url.parse(req.url).pathname;
+        pathname = pathname.replace(/\//, '');
+        if (pathname === 'login' || pathname === 'register') {
+            router[pathname](res);
+        } else {
+            res.write('404');
+        }
+        res.end();
+    }
+}).listen(8080);
+
+console.log('Server is running at port 8080');
