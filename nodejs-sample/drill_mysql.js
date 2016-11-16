ccf6f0910060b088d5f3e722cd2a936bfaadcc38
Alvin1992 <fanrx_1992@163.com>
2016-11-16 13:53:24 +0800

    a drill

---
+++ nodejs-sample/drill_mysql.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Alvin on 2016/11/16.
+ */
+
+var http = require('http');
+var router = require('./modules/routerfile');
+var url = require('url');
+
+http.createServer(function (req, res) {
+    if (req.url !== '/favicon.ico') {
+        var pathname = url.parse(req.url).pathname;
+        pathname = pathname.replace(/\//, '');
+        try {
+            router[pathname](req, res);
+        } catch (e) {
+            res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+            res.write(e.toString());
+            res.write('出错了');
+            res.end();
+        }
+    }
+}).listen(8080);
+
+console.log('Server is running at port 8080');
--- nodejs-sample/modules/routerfile.js
+++ nodejs-sample/modules/routerfile.js
@@ -5,6 +5,12 @@
 var fileRouter = require('./file_for_router');
 var url = require('url');
 var querystring = require('querystring');
+var OpPool = require('./opPool');
+
+
+var opPool = new OpPool();
+var pool = opPool.getPool();
+
 
 function getCallback(req, res) {
     res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
@@ -74,5 +80,66 @@ module.exports = {
             }
             fileRouter.asyncRead('./views/dynamicHtml.html', callback);
         });
+    },
+    test_register: function (req, res) {
+        res.writeHead(200, {"Content-Type":'text/plain','charset':'utf-8','Access-Control-Allow-Origin':'*',
+            'Access-Control-Allow-Methods':'PUT,POST,GET,DELETE,OPTIONS'});
+        var post = '';
+        req.on('data', function (chunk) {
+            post += chunk;
+        });
+        req.on('end', function () {
+            post = querystring.parse(post);
+            var param = [post['name'], post['pwd']];
+            console.log(param);
+
+            pool.getConnection(function (err, conn) {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+                var userAddSql = 'insert into user (uname,pwd) values(?,?)';
+                conn.query(userAddSql, param, function (err, rs) {
+                    if (err) {
+                        res.write('0');
+                        res.end();
+                        console.log('insert error: ', err.message);
+                        return;
+                    }
+                    console.log('insert success');
+                    res.write('1');
+                    res.end();
+                    conn.release(); // 放回连接池
+                });
+            });
+
+        });
+    },
+    test_search: function (req, res) {
+        res.writeHead(200, {"Content-Type":'text/json','charset':'utf-8','Access-Control-Allow-Origin':'*',
+            'Access-Control-Allow-Methods':'PUT,POST,GET,DELETE,OPTIONS'});
+        pool.getConnection(function (err, conn) {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            var arr = [];
+            conn.query('SELECT * from user', function (err, rs) {
+                if (err) {
+                    console.log('[query] - : ' + err);
+                    return;
+                }
+                for (var i = 0; i < rs.length; i++) {
+                    arr.push(rs[i]);
+                }
+                res.write(JSON.stringify(arr));
+                res.end();
+                conn.release(); // 放回连接池
+            });
+        });
+    },
+    link_mysql: function (req, res) {
+        var callback = getCallback(req, res);
+        fileRouter.asyncRead('./views/link_mysql.html', callback);
     }
 };
